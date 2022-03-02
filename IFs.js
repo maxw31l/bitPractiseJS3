0c8c0e72f852881cebd5a99f3e09dd10d37452b9
Egidijus Vaitkevicius <93944038+hitbWinning@users.noreply.github.com>
2022-03-02 11:05:20 +0200

    Add files via upload

---
+++ IFs.js
@@ -0,0 +1,312 @@
+/*
+1.Žinomas skaičius n. Parašykite programą, kuri nustatytų ar skaičius yra blogas ar geras? Neigiamas –„Blogas“, teigiamas „Geras“. Pasitikrinkite: Kai n = 20, turėtume gauti: Skaičius geras.
+2.Mokinys eina gatve ir prieina sankryžą reguliuojamą šviesoforu. Sudarykite programą, kuri nustatytu, kaip turi pasielgti mokinys? Žinome šviesoforo spalvos kodą n, 1-žalia, 2-geltona, 3-raudona. Pasitikrinkite: Kai n= 2, turėtume gauti: Palaukite.
+3.Gintautas planuoja persikraustyti į naujus namus ir jam reikia sudėti visas turimas knygas į dėžes. Gintautas turi d dėžių ir k knygų. Parašykite programą, kuri apskaičiuotų, ar Gintauto turimos knygos tilps į dėžes, jei į vieną dėžę telpa po n knygų. Pasitikrinkite: Kai d=2, k=8, n=5 tuomet ekrane turi būti rodomas pranešimas: Knygos telpa į dėžes; kai d=3, k=18, n=5 tuomet ekrane turi būti rodomas pranešimas: Knygos netelpa į dėžes;
+4.Skaičius skaitomas skaitmenimis iš abiejų galų vienodai vadinamas polindromu. Pvz.: 121. Žinomas 4-klis natūralus skaičius n. Nustatykite ar skaičius yra polindromas. Pasitikrinkite: Kai n = 1221 , turėtume gauti: Skaičius polindromas; kai n=1223 tada Skaičius nepolindromas
+5.Loterijos bilietą sudaro šešiaženklis numeris. Loterijoje laimi tas bilietas, kurio numerio skaitmenų suma dalosi iš 4. Nustatykite, ar loterijos bilietas n yra laimingas?. Pasitikrinkite: Kai n = 222442, turėtume gauti: Bilietas laimingas
+6.Keliamiejimetai turi 366 dienas, o paprastieji 365. Visi metai, išskyrus šimtmečius, yra keliamieji, jei dalūs iš 4. Pasakykite, ar metai m yra keliamieji ar paprastieji? Pasitikrinkite: Kai m = 2004, turi būti spausdinama: Metai keliamieji.
+7.Skaičius skaitomas skaitmenimis iš abiejų galų vienodai vadinamas polindromu. Pvz.: 121. Žinomas 5-klis natūralus skaičius n. Nustatykite ar skaičius yra polindromas. Pasitikrinkite: Kai n = 12321 , turėtume gauti: Skaičius polindromas; kai n=12423 tada Skaičius nepolindromas
+8.Du broliai nesutaria, kuriam tvarkyti kambarį. Jie meta kauliuką. Jei iškrenta skaičius 1, 3 arba 5 –kambarį tvarko jaunėlis, jei 2, 4 arba 6 –vyresnėlis. Parašykite programą, kuri įvedus iškritusį skaičių k, ekrane rodytų pranešimą „Kambarį tvarkys jaunėlis“, arba „Kambarį tvarkys vyresnėlis“. Pasitikrinkite: kai k = 1, ekrane turi būti: Kambarį tvarkys jaunėlis. Kai k = 4, ekrane turi būti: Kambarį tvarkys vyresnėlis.
+9.Žinomi trijų klasės draugų kontrolinio darbo pažymiai. Sudarykite programą, kuri nustatytų, kuris mokinys gavo geriausią pažymį, kai žinome jų pažymius p1, p2, p3 Pasitikrinkite: Jei p1=5, p2=8, p3=9 ekrane turi būti: Trečias gavo geriausią pažymį.
+10.Parašykite programą, kuri įvedus mėnesio numerį n atspausdintų to mėnesio dienų kiekį d. (tarkime, kad metai yra paprastieji) (pvz. n=5, ats. d=31; n=2 ats. d=28)
+11.Petras išėjo iš namų, kai laikrodis rodė v1 valandų ir m1 minučių. Į gimnaziją Petro kelionė trunka m2 minučių. Parašykite programą, kuri ekrane parodytų pranešimą apie tai, ar Petrasnepavėluos į pamoką, prasidedančią v valandų ir m minučių. Pasitikrinkite: jei v1 = 8, m1 = 29, m2 = 43, v = 9, m = 5, turi būti: Petras į pamoką pavėluos; jei v1 = 8, m1 = 29, m2 = 23, v = 9, m = 5, turi būti: Petras į pamoką nepavėluos.
+12.Yra n degtukų. Parašykite programą, kuri nustatytų, ar iš tų degtukų galima sudėti lygiakraštį trikampį. Dėliojamai figūrai turi būti panaudoti visi degtukai. Degtukų laužyti negalima. Pasitikrinkite: jei n = 6, turi būti: Trikampį sudėlioti galima. jei n = 4, turi būti:Trikampį sudėlioti negalima.
+13.Pirmosios olimpinės žaidynės įvyko 1896 metais ir toliau organizuojamos kas ketveri metai. Jei žaidynės neįvyksta, tie metai vis tiek laikomi olimpiniais, o žaidynėms skiriamas eilės numeris. Parašykite programą, kuri surastųm-ųjų metų olimpinių žaidynių numerį n. Jei metai neolimpiniai, turi būti spausdinama „Metai neolimpiniai“. Pasitikrinkite. Kai m = 1904, turi būti spausdinama: n = 3. Kai m = 2005, turi būti spausdinama: Metai neolimpiniai.
+14.Žinomi kvadratinės lygties ax2 +bx+c=0 koeficientai a, b, c. Parašykite programą, kuri rastų lygties sprendinius. Pasitikrinkite:a=1, b=2, c=1, kompiuterio ekrane turi būti rodomas rezultatas: vienas sprendinys 1.00 a=1, b=4, c=3, kompiuterio ekrane turi būti rodomas rezultatas: du sprendiniai 3.00 1.00 a=1, b=3, c=4, kompiuterio ekrane turi būti rodomas rezultatas: nėra sprendinių
+15.Saulius sutaupė c centų ir už juos nusprendė nusipirkti ledų. Parduotuvėje porcija ledų kainuoja p centų. Parašykite programą, kuri suranda, kiek porcijų ledų k nusipirks Saulius ir kiek centų m dar liks. Pasitikrinkite Kai c=50, p=20, turi būti : Saulius nusipirks 2 porcijas, liks centų 10
+16.Vairuotojas iš sandėlio į parduotuvę turi pervežti n dėžių prekių. Į mašiną telpa m dėžių prekių. Sukurkite programą, kuri apskaičiuotų ir kompiuterio ekrane parodytų, kiek kartų k turės nuvažiuoti vairuotojas į sandėlį, kad parvežtų visas prekių dėžes į parduotuvę. Pasitikrinkite: jei n = 100, m = 14, tai k = 8.
+17.Duoti du sveikieji skaičiai a ir b. Parašykite programą, kuri didesnį skaičių pamažintu vienetu, o mažesnį padidintu vienetu.
+
+1.1 Parašykite programą, kuri apskaičiuotų, kiek knygų k vidutiniškai per metus perskaito vienas mokyklos bibliotekos lankytojas. Žinomas vidutiniškai per vieną mėnesį perkaitytų knygų skaičius v ir vidutiniškai per metus apsilankiusiųjų bibliotekoje skaičius n. Pasitikrinkite: Įvedę v = 120 , n = 800, turėtume gauti: k = 2;
+2.1 Parašykite programą, kuri apskaičiuotų, kiek vidutiniškai keleivių k važiuoja į Vilnių viename traukinio vagone, jei žinomas traukinio keleivių skaičius n, keleivių vykstančių ne į Vilnių, skaičius m ir vagonų skaičius v. Pasitikrinkite: Įvedę n = 100 , m = 20 ir v = 4, turėtume gauti: k = 20;
+
+*/
+
+// ! 1.Žinomas skaičius n. Parašykite programą, kuri nustatytų ar skaičius yra blogas ar geras? Neigiamas –„Blogas“, teigiamas „Geras“. Pasitikrinkite: Kai n = 20, turėtume gauti: Skaičius geras.
+
+
+// let skaicius = 0
+// if(skaicius<0){
+//     console.log("skaičius yra blogas");
+// }
+// else if(skaicius>0) {
+//     console.log("skaičius yra geras");
+// } else {
+//     console.log("skaičius lygus nuliui");
+// }
+
+// ! 2. Mokinys eina gatve ir prieina sankryžą reguliuojamą šviesoforu. Sudarykite programą, kuri nustatytu, kaip turi pasielgti mokinys? Žinome šviesoforo spalvos kodą n, 1-žalia, 2- geltona, 3-raudona. Pasitikrinkite: Kai n= 2, turėtume gauti: Palaukite.
+
+
+
+// a = 2;
+
+// if(a == 2){
+//     console.log("Palaukite")
+// } else if (a == 1) {
+//     console.log("Eikite")
+// } else if (a == 3) {
+//     console.log("Stokite")
+// } else {
+//     console.log("Sviesaforas daugiau spalvų neturi")
+// }
+
+// ! 3.Gintautas planuoja persikraustyti į naujus namus ir jam reikia sudėti visas turimas knygas į dėžes. 
+// Gintautas turi d dėžių ir k knygų. Parašykite programą, kuri apskaičiuotų, ar Gintauto turimos knygos tilps į dėžes, jei į vieną dėžę telpa po n knygų. Pasitikrinkite: Kai d=2, k=8, n=5 tuomet ekrane turi būti rodomas pranešimas: Knygos telpa į dėžes; kai d=3, k=18, n=5 tuomet ekrane turi būti rodomas pranešimas: Knygos netelpa į dėžes;
+// d=2, k=8, x=5 
+
+// x = 5
+// d = 3
+// k = 15
+// let kTalpa= d*x
+
+// if(kTalpa>=k) {
+//     console.log("Knygos telpa į dežes");
+// } 
+// else {
+//    console.log("Kygos netelpa į dežes");
+// } 
+
+
+// ! 4. Skaičius skaitomas skaitmenimis iš abiejų galų vienodai vadinamas polindromu. Pvz.: 121. Žinomas 4-klis natūralus skaičius n. Nustatykite ar skaičius yra polindromas. Pasitikrinkite: Kai n = 1221 , turėtume gauti: Skaičius polindromas; kai n=1223 tada Skaičius nepolindromas
+
+// w=5225
+
+// let fourth = w % 10;
+// console.log(fourth);
+
+// let third = Math.floor(w / 10) % 10;
+// console.log(third);
+
+// let second = Math.floor(w / 100) % 10;
+// console.log(second);
+
+// let first = Math.floor(w / 1000);
+// console.log(first);
+
+// if (first == fourth && second == third) {
+// console.log("Skaicius yra poligromas");
+// } else {
+//     console.log("skaicius nera poligromas");
+// }
+
+
+// ! 5.Loterijos bilietą sudaro šešiaženklis numeris. Loterijoje laimi tas bilietas, kurio numerio skaitmenų suma dalosi iš 4. Nustatykite, ar loterijos bilietas n yra laimingas?. Pasitikrinkite: Kai n = 222442, turėtume gauti: Bilietas laimingas
+
+// let s = 22246;
+// let sesi = s % 10;
+// let penki = Math.floor(s / 10) % 10;
+// let keturi = Math.floor(s / 100) % 10;
+// let keturi = Math.floor(s / 1000) % 10;
+// let keturi = Math.floor(s / 10000) % 10;
+// let keturi = Math.floor(s / 100000) % 10;
+// let sums = sesi + penki + keturi + trys + du + vienas;
+
+// if (sums % 4 == 0) {
+//     console.log("Bilietas laimingas");
+// } else {
+//     console.log("Bilietas nelaimingas");
+// }
+
+/*
+Math.Floor() - Apvalina skaičių į mažąją pusę - 4.9 = 4
+Math.Round() - Apvalina skaičių pagal matematines taisykles - 4.5 = 5
+Match.ceil() - Apvalina skaičius į didžiąją pusę 4.1 = 5
+&&(AND) if(a === 0 && b === 5){then....}
+% 17/5=3.4 17 = 3*5+2 17 = 2*6+5 
+|| (OR) if(a === 0 || b === 5){then....}
+No ! (NOT) IF (a !== 0 && a !== 5){}
+*/
+
+//! 6.Keliamiejimetai turi 366 dienas, o paprastieji 365. Visi metai, išskyrus šimtmečius, yra keliamieji, jei dalūs iš 4. Pasakykite, ar metai m yra keliamieji ar paprastieji? Pasitikrinkite: Kai m = 2004, turi būti spausdinama: Metai keliamieji.
+
+// m = 2000;
+
+// if (m % 4 === 0 && m % 100 != 0) {
+//     console.log("Metai keliamieji");
+// } 
+// else {
+//     console.log("Metai nekeliamieji");
+// }
+
+
+//! 7.Skaičius skaitomas skaitmenimis iš abiejų galų vienodai vadinamas polindromu. Pvz.: 121. Žinomas 5-klis natūralus skaičius n. Nustatykite ar skaičius yra polindromas. Pasitikrinkite: Kai n = 12321 , turėtume gauti: Skaičius polindromas; kai n=12423 tada Skaičius nepolindromas
+
+// let h = 12321;
+
+// let t1 = h % 10;
+// let t2 = Math.floor (h/10) % 10;
+// let t3 = Math.floor (h/100) % 10;
+// let t4 = Math.floor (h/1000) % 10;
+// let t5 = Math.floor (h/10000) % 10;
+
+// if (t1 == t5 && t2 == t4) {
+//     console.log ("Skaičius polindromas")
+// } else {
+//     console.log ("Skaičius nepilndromas");
+// }
+
+//! 8.Du broliai nesutaria, kuriam tvarkyti kambarį. Jie meta kauliuką. Jei iškrenta skaičius 1, 3 arba 5 –kambarį tvarko jaunėlis, jei 2, 4 arba 6 –vyresnėlis. Parašykite programą, kuri įvedus iškritusį skaičių k, ekrane rodytų pranešimą „Kambarį tvarkys jaunėlis“, arba „Kambarį tvarkys vyresnėlis“. Pasitikrinkite: kai k = 1, ekrane turi būti: Kambarį tvarkys jaunėlis. Kai k = 4, ekrane turi būti: Kambarį tvarkys vyresnėlis.
+
+// let k = 67;
+
+// if (k<= 6 && k>=1){
+//     if(k % 2 === 0){
+//         console.log("Vyresnėlis");
+//     }    else {
+//             console.log("Jaunėlis");
+//         }
+//     } else {
+//         console.log("Kauliukas turi šėšias akutes");
+//     }
+
+
+//! 9.Žinomi trijų klasės draugų kontrolinio darbo pažymiai. Sudarykite programą, kuri nustatytų, kuris mokinys gavo geriausią pažymį, kai žinome jų pažymius p1, p2, p3 Pasitikrinkite: Jei p1=5, p2=8, p3=9 ekrane turi būti: Trečias gavo geriausią pažymį.
+
+// let p1= 500;
+// let p2 = 80;
+// let p3 = 10;
+
+// if (p1 > p2 && p3 < p1 ) {
+//     console.log("pirmas geriauisas");
+// } else if  (p2 > p1 & p3 < p2)
+//     console.log("antras geriausias");
+//  else {
+//     console.log ("trečias geriausias");
+// }
+// ! 10.Parašykite programą, kuri įvedus mėnesio numerį n atspausdintų to mėnesio dienų kiekį d. (tarkime, kad metai yra paprastieji) (pvz. n=5, ats. d=31; n=2 ats. d=28)
+
+
+// let n = 12; 
+// if (n>0 &&  n<13){
+//     if( n === 2){
+//         console.log(28);
+//     } else if ( n=== 4 || n === 6 || n === 9 || n === 11) {
+//         console.log(30);
+//     } else if (n === 1 || n === 3 || n === 5 || n === 7 || n === 8 || n === 10 || n === 12){
+//         console.log(31);
+//     } else {
+//         console.log("Tokio mėnesio nėra");
+//     }
+// }
+
+// ! 11.Petras išėjo iš namų, kai laikrodis rodė v1 valandų ir m1 minučių. Į gimnaziją Petro kelionė trunka m2 minučių. Parašykite programą, kuri ekrane parodytų pranešimą apie tai, ar Petrasnepavėluos į pamoką, prasidedančią v valandų ir m minučių. Pasitikrinkite: jei v1 = 8, m1 = 29, m2 = 43, v = 9, m = 5, turi būti: Petras į pamoką pavėluos; jei v1 = 8, m1 = 29, m2 = 23, v = 9, m = 5, turi būti: Petras į pamoką nepavėluos.
+
+// let v = 8;
+// let m = 30;
+// let v1 = 8;
+// let m1 = 0;
+// let m2 = 30;
+// if (v*60+m >= v1*60+m1+m2){
+//     console.log("Petras nepaveluos")
+// } else {
+//     console.log("Petras paveluos")
+// } 
+
+// ! 12.Yra n degtukų. Parašykite programą, kuri nustatytų, ar iš tų degtukų galima sudėti lygiakraštį trikampį. Dėliojamai figūrai turi būti panaudoti visi degtukai. Degtukų laužyti negalima. Pasitikrinkite: jei n = 6, turi būti: Trikampį sudėlioti galima. jei n = 4, turi būti:Trikampį sudėlioti negalima.
+
+
+// let n = 27; 
+
+// if ((n % 3 === 0) && (n != 0)){
+//     console.log("Trikampį sudaryti galima");
+// } else {
+//     console.log("Trikampio sudėlioti negalima");
+// }
+
+// ! 13.Pirmosios olimpinės žaidynės įvyko 1896 metais ir toliau organizuojamos kas ketveri metai. Jei žaidynės neįvyksta, tie metai vis tiek laikomi olimpiniais, o žaidynėms skiriamas eilės numeris. Parašykite programą, kuri surastųm-ųjų metų olimpinių žaidynių numerį n. Jei metai neolimpiniai, turi būti spausdinama „Metai neolimpiniai“. Pasitikrinkite. Kai m = 1904, turi būti spausdinama: n = 3. Kai m = 2005, turi būti spausdinama: Metai neolimpiniai.
+
+// let y=1904;
+// let oly=1896;
+
+// if ((y-oly)%4===0){
+//     console.log(((y-oly)/4)+1);
+// } else {
+//     console.log("Metai neolimpiniai")
+// }
+
+//! 14.Žinomi kvadratinės lygties ax2 +bx+c=0 koeficientai a, b, c. Parašykite programą, kuri rastų lygties sprendinius. Pasitikrinkite:a=1, b=2, c=1, kompiuterio ekrane turi būti rodomas rezultatas: vienas sprendinys 1.00 a=1, b=4, c=3, kompiuterio ekrane turi būti rodomas rezultatas: du sprendiniai 3.00 1.00 a=1, b=3, c=4, kompiuterio ekrane turi būti rodomas rezultatas: nėra sprendinių
+
+
+// let a14 = 1;
+// let b14 = 4;
+// let c14 = 3;
+// let d14 = (b14*b14)-(4*a14*c14);
+// console.log (d14);
+
+// let spr =-b14/(2*a14);
+
+// let spr1 = ((-b14)+Math.sqrt(d14))/(2*a14);
+// let spr2 = ((-b14)-Math.sqrt(d14))/(2*a14);
+
+// if(d14>0){
+//     console.log("Du sprendiniai"+" " + spr1+ " "+spr2);
+// } else if (d14 === 0){
+//     console.log("vienas sprendinys "+ spr);
+// } else {
+//     console.log("nėra sprendinių");
+// }
+
+//! 15.Saulius sutaupė c centų ir už juos nusprendė nusipirkti ledų. Parduotuvėje porcija ledų kainuoja p centų. Parašykite programą, kuri suranda, kiek porcijų ledų k nusipirks Saulius ir kiek centų m dar liks. Pasitikrinkite Kai c=50, p=20, turi būti : Saulius nusipirks 2 porcijas, liks centų 10
+
+//  let c15 = 50;
+//  let p15 = 20;
+//  let k15 = Math.floor(c15 / p15);
+
+//  if (c15 >= p15) {
+//      console.log("Saulius pirks" + " " + k15 + " porcijas, liks centu" + " " + (c15 - (k15 * p15)));
+//  } else {
+//      console.log("Neužtenka pinigų");
+//  }
+
+//! 16.Vairuotojas iš sandėlio į parduotuvę turi pervežti n dėžių prekių. Į mašiną telpa m dėžių prekių. Sukurkite programą, kuri apskaičiuotų ir kompiuterio ekrane parodytų, kiek kartų k turės nuvažiuoti vairuotojas į sandėlį, kad parvežtų visas prekių dėžes į parduotuvę. Pasitikrinkite: jei n = 100, m = 14, tai k = 8.
+
+// let n16 = 100;
+// let m16 = 14;
+
+// if (n16 > 0 && m16 >0){
+//     console.log(Math.ceil(n16 / m16));
+// } else {
+//     console.log("Nėra ką arba kuom vežti")
+// }
+
+
+//! 17.Duoti du sveikieji skaičiai a ir b. Parašykite programą, kuri didesnį skaičių pamažintu vienetu, o mažesnį padidintu vienetu.
+
+
+// let a17 = 4;
+// let b17 = 4;
+
+// if (a17 > b17) {
+//     console.log("a -" + (a17 - 1)+ " " + "b - " + (b17 + 1));
+// } else if (b17 > a17) {
+//     console.log("b - " + (b17 - 1)+ " " + "a - " + (a17 + 1));
+// } else {
+//     console.log(" Skaičiai lygūs")
+// }
+
+//! 1.1 Parašykite programą, kuri apskaičiuotų, kiek knygų k vidutiniškai per metus perskaito vienas mokyklos bibliotekos lankytojas. Žinomas vidutiniškai per vieną mėnesį perkaitytų knygų skaičius v ir vidutiniškai per metus apsilankiusiųjų bibliotekoje skaičius n. Pasitikrinkite: Įvedę v = 120 , n = 800, turėtume gauti: k = 2;
+
+
+// v = 120;
+// yearlyv = v*12;
+// n = 800;
+// monthlyv = n/12;
+
+// k = (yearlyv / monthlyv);
+//     console.log(k);
+
+
+
+
+//! 2.1 Parašykite programą, kuri apskaičiuotų, kiek vidutiniškai keleivių k važiuoja į Vilnių viename traukinio vagone, jei žinomas traukinio keleivių skaičius n, keleivių vykstančių ne į Vilnių, skaičius m ir vagonų skaičius v. Pasitikrinkite: Įvedę n = 100 , m = 20 ir v = 4, turėtume gauti: k = 20;
+
+// let n = 100;
+// let m = 20;
+// let v = 4;
+
+// k = (n-m)/v;
+// console.log(k);
+
+
+
